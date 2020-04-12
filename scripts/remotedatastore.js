9050e268c9082e776bf64c72fc555e2813d333ac
mulvaneya7 <mulvaneya7@gmail.com>
2020-04-12 14:43:04 -0700

    Added firebase firestore for coffee orders

--- scripts/remotedatastore.js
+++ scripts/remotedatastore.js
@@ -3,17 +3,52 @@
     var App = window.App || {};
     var $ = window.jQuery;
   
-    function RemoteDataStore(url) {
-      if (!url) {
+    function RemoteDataStore(url) {  
+        if (!url) {
         throw new Error('No remote URL supplied.');
-      }
-  
-      this.serverUrl = url;
+        }
+
+        this.serverUrl = url;
+        // Your web app's Firebase configuration
+        var firebaseConfig = {
+        apiKey: "AIzaSyDIeLhOzbFDwv00Lr9bZioppS3LdDtnHjE",
+        authDomain: "coffeerun-40edf.firebaseapp.com",
+        databaseURL: "https://coffeerun-40edf.firebaseio.com",
+        projectId: "coffeerun-40edf",
+        storageBucket: "coffeerun-40edf.appspot.com",
+        messagingSenderId: "816320380057",
+        appId: "1:816320380057:web:87497e317e069b15084c08"
+        };
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+        //reference to database
+        this.database = firebase.firestore();
+
     }
 
     RemoteDataStore.prototype.add = function (key,val) {
+        let db = this.database.collection("coffeeOrders");
+        console.log(db);
+        db.add({
+            coffee: val.coffee,
+            email: val.emailAddress,
+            flavor: val.flavor,
+            size: val.size,
+            strength: val.strength
+        })
+        .then(function(docRef) {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(function(error) {
+            console.error("Error adding document: ", error);
+        });
+
+
+
+
         $.post(this.serverUrl, val, function (serverResponse) {
             console.log(serverResponse);
+            
         });
     };
 
